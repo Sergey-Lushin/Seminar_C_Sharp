e90ec796046ab104f0f520e5e1b66ae0c049c1b4
Sergey <llan@ya.ru>
2023-02-02 15:02:55 +0300

    Домашняя работа к семинару 9

--- Homework9/Program.cs
+++ Homework9/Program.cs
@@ -37,14 +37,11 @@ else{
         Console.WriteLine(Ackermann(M, N));
     }
 //------метод-------
-int Ackermann(int m, int n)
-		{
-			if(m == 0)
-			{
+int Ackermann(int m, int n){
+			if(m == 0){
 				return n + 1;
 			}
-			if(m > 0 && n == 0)
-			{
+			if(m > 0 && n == 0){
 				return Ackermann(m - 1, 1);
 			}
 			return Ackermann(m - 1, Ackermann(m, n - 1));
